149af8d89171bbe58e780b15bf4ebd210626c17e
Kelvin <kwardeu@gmail.com>
2020-10-31 13:17:50 +0000

    FEAT: Delete reviews. MINOR: Styling for viewreviews.html

--- app.py
+++ app.py
@@ -5,18 +5,14 @@ from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
-
+# Create flask app and configure database
 app = Flask(__name__)
-
-MONGO_URI = os.environ.get("MONGO_URI")
-DATABASE = "book_app_db"
-
-app.config["MONGO_DBNAME"] = DATABASE
-app.config["MONGO_URI"] = MONGO_URI
-
+app.config["MONGO_DBNAME"] = "book_app_db"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 
+# -------------------------------------------------- Book realted views
 @app.route('/')
 @app.route('/view_books')
 def view_books():
@@ -32,14 +28,37 @@ def add_book():
 def insert_book():
     formResults = request.form.to_dict()
     # Format form results before submitting to database
-    for k, v in formResults.items():
-        if k != 'summary':
-            formResults[k] = v.lower()
-
+    formResults['title'] = formResults['title'].lower()
+    formResults['author'] = formResults['author'].lower()
+    # Initialise variables not included in form
     formResults['rating'] = 0
-    formResults['reviews'] = 0
+    formResults['reviews'] = []
     mongo.db.books.insert(formResults)
     return redirect(url_for('view_books'))
+
+
+@app.route('/edit_book/')
+def edit_book():
+    return render_template('editbook.html', books=mongo.db.books.find())
+
+
+@app.route('/update_book/<book_id>', methods=['POST'])
+def update_book(book_id):
+    return redirect(url_for('view_books'))
+
+
+# -------------------------------------------------- Review realted views
+@app.route('/view_reviews/<book_id>')
+def view_reviews(book_id):
+    reviewInfo = mongo.db.books.find_one({'_id': ObjectId(book_id)},
+                                         {'title': (),
+                                          'rating': (),
+                                          'reviews': (),
+                                          '_id': 0})
+    bookTitle = reviewInfo['title']
+    bookRating = reviewInfo['rating']
+    reviews = reviewInfo['reviews']
+    return render_template('viewreviews.html', reviews=reviews, bookTitle=bookTitle, bookRating=bookRating, book_id=book_id)
 
 
 @app.route('/add_review/<book_id>')
@@ -50,33 +69,61 @@ def add_review(book_id):
 @app.route('/insert_review', methods=['POST'])
 def insert_review():
     formResults = request.form.to_dict()
-    # Format reviewer name
-    formResults['name'] = formResults['name'].lower()
+    formResults['name'] = formResults['name'].lower()  # Format reviewer name
+
     # Stop rating average manipulation
     formResults['rating'] = int(formResults['rating'])
-    if formResults['rating'] > 5:
-        formResults['rating'] = 5
+    if formResults['rating'] > 10:
+        formResults['rating'] = 10
     if formResults['rating'] < 0:
         formResults['rating'] = 0
-    
-    # Increment number of reviews books.book._id has
-    bookID = formResults['book_id']
-    reviews = mongo.db.books.find_one({"_id": bookID})
-    print(type(bookID))
-    print('##############################################')
-    for k, v in formResults.items():
-        print(k, v)
-    print(reviews)
 
+    # Update average book score
+    update_average_score(formResults['book_id'], formResults['rating'])
 
-    mongo.db.books.update({'_id': formResults['book_id']}, {"$set": {"reviews": 5}})
-    # Update review collection with new review
-    mongo.db.reviews.insert(formResults)
+    # Update book with new review
+    mongo.db.books.update_one({'_id': ObjectId(formResults['book_id'])},
+                              {"$push": {'reviews':
+                                         {'_id': ObjectId(),
+                                          'reviewer': formResults['name'],
+                                          'rating': formResults['rating'],
+                                          'review': formResults['review']}}})
     return redirect(url_for('view_books'))
+
+
+@app.route('/delete_review/<book_id>/<review_id>')
+def delete_review(book_id, review_id):
+    print('################################')
+    print(book_id)
+    print(review_id)
+
+    #findId = mongo.db.books.aggregate([{"$project": { "matchedIndex": { "$indexOfArray": [ "reviews", 1]}}}])
+
+    mongo.db.books.update_one({'_id': ObjectId(book_id)},
+                          {'$pull': {'reviews':
+                                     {'_id': ObjectId(review_id)}}})
+
+    #mongo.db.books.update_many({'_id': ObjectId(book_id)}, { '$pull': { 'reviews': { '_id': { '$elemMatch': { '_id': review_id}}}}})
+    update_average_score(book_id, 0)
+    return redirect(url_for('view_reviews', book_id=book_id))
+
+def update_average_score(book_id, thisReviewRating=0):
+    # Get new avarage score from all reviews of book and update database
+    # Takes book_id as string and rating
+    currentBookReviews = mongo.db.books.find_one({'_id': ObjectId(book_id)},
+                                                 {'reviews': (),
+                                                  '_id': 0})['reviews']
+    totalReviews = len(currentBookReviews) + 1
+    totalRating = 0
+    for review in currentBookReviews:
+        totalRating += int(review['rating'])
+    totalRating += thisReviewRating
+    newRating = round(totalRating / totalReviews)
+    mongo.db.books.update_one({'_id': ObjectId(book_id)},
+                              {'$set': {'rating': newRating}})
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')), 
+            port=int(os.environ.get('PORT')),
             debug=True)
-
