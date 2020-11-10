e47b7568be3c0800eee44ceada473200e3767c85
Kelvin <kwardeu@gmail.com>
2020-11-10 13:49:38 +0000

    TESTING: Create more route tests

--- testbookapp.py
+++ testbookapp.py
@@ -11,8 +11,9 @@ app.config.update({
     'WTF_CSRF_ENABLED': False,
     'MONGO_URI': 'mongodb://localhost:27017/testBookDb'})
 
-mongo = PyMongo(app)  # Setup mongoDB with Flaskapp
-appModule.mongo = mongo  # Inject this new database into app.py
+mongo = PyMongo(app)  # setup PyMongo object
+appModule.mongo = mongo  # overwrite app.py mongo object with this modifed version
+
 
 class MongoDbTests(unittest.TestCase):
 
@@ -22,7 +23,8 @@ class MongoDbTests(unittest.TestCase):
 
 
     def test_insert_book(self):
-        response = self.app.post('insert_book', follow_redirects=True, data=dict(title= 'Title Test',
+        # Test when book inserted its contents appears on viewbooks.html
+        self.app.post('insert_book', follow_redirects=True, data=dict(title= 'Title Test',
                                                                author= 'Author Test',
                                                                rating= '5',
                                                                reviews= [],
@@ -30,14 +32,12 @@ class MongoDbTests(unittest.TestCase):
                                                                buy_link= 'www.testbuylink.com',
                                                                genre= 'test genre',
                                                                summary= 'test summary'))
-        print(response)
-
-    def test_index(self):
-        # Check index loads with content
         response = self.app.get("/")
-        self.assertEqual(response.status_code, 200)
-        # Turn searched message into bytes literal and find in response
-        self.assertTrue(b'Click a cover for more info' in response.data)
+        self.assertTrue(b'Title Test' in response.data)
+        self.assertTrue(b'Author Test' in response.data)
+        self.assertTrue(b'www.testlink.com' in response.data)
+        self.assertTrue(b'www.testbuylink.com' in response.data)
+        self.assertTrue(b'test summary' in response.data)
 
 
 class AppRouteTests(unittest.TestCase):
@@ -45,13 +45,27 @@ class AppRouteTests(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client(self)
 
-    def test_index(self):
+    def test_index_page(self):
         # Check index loads with content
         response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
         # Turn searched message into bytes literal and find in response
         self.assertTrue(b'Click a cover for more info' in response.data)
 
+    def test_add_book_page(self):
+        # Check index loads with content
+        response = self.app.get("/add_book")
+        self.assertEqual(response.status_code, 200)
+        # Turn searched message into bytes literal and find in response
+        self.assertTrue(b'Add a book' in response.data)
+
+   ''' def test_add_review_page(self):
+        # Check index loads with content
+        response = self.app.get("/add_review")
+        self.assertEqual(response.status_code, 200)
+        # Turn searched message into bytes literal and find in response
+        self.assertTrue(b'Write a review for' in response.data)
+'''
 
 class TestReviewScoreValidation(unittest.TestCase):
 
