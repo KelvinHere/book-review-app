2f69535135fed599638fccf2b182d3053663318b
Kelvin <kwardeu@gmail.com>
2020-11-10 11:23:24 +0000

    TESTING: testbookapp.py can write to test database

---
+++ local.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+print('##################')
+appt = Flask(__name__)
+appt.config["MONGO_DBNAME"] = "book_app_db"
+appt.config["MONGO_URI"] = 'mongodb://localhost:27017/testBookDb'
+mongo = PyMongo(appt)
+
+appt.config.update({
+    'TESTING': True,
+    'WTF_CSRF_ENABLED': False
+    })
+
+#mongo.db.books.insert_one({'TestTitle': 'TitleContent'})
+books = mongo.db.books.find()
+for each in books:
+    print(each)
+colls = mongo.db.list_collection_names()
+for each in colls:
+    print(each)
+
+#print(mongo.db.books.find())
--- testbookapp.py
+++ testbookapp.py
@@ -1,15 +1,54 @@
 import unittest
 import app as appModule
+from flask_pymongo import PyMongo
 
+#Grap book review app
 app = appModule.app
 
 # Set app to use local mongoDB database
 app.config.update({
     'TESTING': True,
     'WTF_CSRF_ENABLED': False,
-    'MONGO_URI': 'mongodb://localhost:27017/'})
+    'MONGO_URI': 'mongodb://localhost:27017/testBookDb'})
 
-print(app.config['MONGO_URI'])
+mongo = PyMongo(app)  # Setup mongoDB with Flaskapp
+appModule.mongo = mongo  # Inject this new database into app.py
+
+
+###Commands to check test database is working
+#mongo.db.books.insert_one({'TestTitle': 'TitleContent'})
+#books = mongo.db.books.find()
+#for each in books:
+#    print(each)
+#colls = mongo.db.list_collection_names()
+#for each in colls:
+#    print(each)
+
+
+class MongoDbTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client(self)
+        mongo.db.books.delete_many({})
+
+
+    def test_insert_book(self):
+        response = self.app.post('insert_book', follow_redirects=True, data=dict(title= 'Title Test',
+                                                               author= 'Author Test',
+                                                               rating= '5',
+                                                               reviews= [],
+                                                               link= 'www.testlink.com',
+                                                               buy_link= 'www.testbuylink.com',
+                                                               genre= 'test genre',
+                                                               summary= 'test summary'))
+        print(response)
+
+    def test_index(self):
+        # Check index loads with content
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        # Turn searched message into bytes literal and find in response
+        self.assertTrue(b'Click a cover for more info' in response.data)
 
 
 class AppRouteTests(unittest.TestCase):
