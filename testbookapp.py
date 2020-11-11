93d6f3956170941f93e01cf51cd26c4f53296130
Kelvin <kwardeu@gmail.com>
2020-11-11 18:45:06 +0000

    TESTING: Add tests for edit_review, add_review and insert_reviews

--- testbookapp.py
+++ testbookapp.py
@@ -1,9 +1,15 @@
+# Unit tests for "Book Review App"
+# These test all views and functions of app.py
+
+
 import unittest
 import app as appModule
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-#Grap book review app
+# Start mongod before running unit tests "mongod --dbpath testdb"
+
+# Grap book review app
 app = appModule.app
 
 # Set app to use local mongoDB database
@@ -49,13 +55,13 @@ class MongoDbTests(unittest.TestCase):
         # Test update_book route can take an ID and update all fields from form
         bookId = get_id_from_cursor(add_book_return_cursor())  # Create a valid book and get _id
         self.app.post(f'update_book/{bookId}', follow_redirects=True, data=dict(title= 'my test title altered',
-                                                               author= 'author test altered',
-                                                               rating= '10',
-                                                               reviews= [],
-                                                               link= 'www.testlinkaltered.com',
-                                                               buy_link= 'www.testbuylinkaltered.com',
-                                                               genre= 'test genre altered',
-                                                               summary= 'test summary altered'))
+                                                               author='author test altered',
+                                                               rating='10',
+                                                               reviews=[],
+                                                               link='www.testlinkaltered.com',
+                                                               buy_link='www.testbuylinkaltered.com',
+                                                               genre='test genre altered',
+                                                               summary='test summary altered'))
         response = self.app.get("/")
         # Assert all relevent book information is present on view books page
         self.assertTrue(b'my test title altered' in response.data)
@@ -72,6 +78,25 @@ class MongoDbTests(unittest.TestCase):
         self.app.get(f"/delete_book/{bookId}")
         books = mongo.db.books.count_documents({'_id': ObjectId(bookId)}) # Check book deleted
         self.assertEqual(books, 0)
+
+    def test_insert_and_view_reviews(self):
+        # Test insert_review can insert review and view_reviews can fetch data
+        bookId = get_id_from_cursor(add_book_return_cursor())  # Create a valid book and get _id
+        self.app.post(f'insert_review', follow_redirects=True, data=dict(book_id=bookId,
+                                                                         name="reviewer test name",
+                                                                         rating="10",
+                                                                         review="sample test review content"))
+        response = self.app.get(f"/view_reviews/{bookId}")
+        # Assert all relevent book information is present on view books page
+        self.assertTrue(b'My Test Title reviews' in response.data)  # Page title
+        self.assertTrue(b'Reviewer Test Name' in response.data)  # Reviewer Name
+        self.assertTrue(b'star-10.png' in response.data)  # Image name for star rating
+        self.assertTrue(b'sample test review content' in response.data)  # Review
+
+
+    def test_updatE_review(self):
+        # Test reviews can be updated from form data by bookId and reviewId
+        
 
 
 class AppRouteTests(unittest.TestCase):
@@ -119,6 +144,16 @@ class AppRouteTests(unittest.TestCase):
         self.assertTrue(b'www.testbuylink.com' in response.data)
         self.assertTrue(b'test summary' in response.data)
 
+    def test_edit_review(self):
+        # Test edit_review can retrieve reviews from DB
+        bookId = get_id_from_cursor(add_book_return_cursor())  # Create a valid book and get _id
+        add_test_review(bookId)
+        book = list(mongo.db.books.find({'_id': ObjectId(bookId)}))
+        reviewId = book[0]['reviews'][0]['_id']
+        response = self.app.get(f"/edit_review/{bookId}/{reviewId}")
+        self.assertTrue(b'Edit this review' in response.data)  # Page title
+        self.assertTrue(b'value="10" selected>5 Star' in response.data)  # Pre selected star rating
+        self.assertTrue(b'sample test review content' in response.data)  # Review
 
 
 class TestReviewScoreValidation(unittest.TestCase):
@@ -139,9 +174,20 @@ def add_book_return_cursor():
                                'reviews': []})
     return mongo.db.books.find({'title': 'my test title'})[0]
 
+
 def get_id_from_cursor(cursor):
     bookId = (cursor['_id'])
     return bookId
+
+
+def add_test_review(bookId):
+    mongo.db.books.update_one({'_id': ObjectId(bookId)},
+                              {"$push": {'reviews':
+                                         {'_id': ObjectId(),
+                                          'reviewer': 'reviewer test name',
+                                          'rating': 10,
+                                          'review': 'sample test review content'}}})
+
 
 #Look inside database TEMP
 #books = mongo.db.books.find()
